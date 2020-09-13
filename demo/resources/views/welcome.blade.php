c18968f88da0a6d9dc2e2983b8583c54a8002024
yolo-lin <yiheng.lin@yolo-japan.co.jp>
2020-09-13 14:52:54 +0900

    add demo

--- demo/resources/views/welcome.blade.php
+++ demo/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel test
+                    Laravel test demo
                 </div>
 
                 <div class="links">
